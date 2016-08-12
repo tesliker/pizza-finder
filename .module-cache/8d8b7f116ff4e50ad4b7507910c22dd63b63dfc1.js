7a54664b46edf3172ae02179d4a490c201b85768
vagrant <vagrant@d8.local>
2016-08-12 21:25:12 +0000

    initial commit

---
+++ .module-cache/8d8b7f116ff4e50ad4b7507910c22dd63b63dfc1.js
@@ -0,0 +1,9 @@
+/**
+ * @file
+ * First example react component.
+ */
+
+ReactDOM.render(
+  React.createElement("h1", null, "Hello, world!"),
+  document.getElementById('react-example-1')
+);
---
+++ src/pizza_finder.js
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * First example react component.
+ */
+
+var Radium = require('radium');
+
+import Radium, { Style } from 'radium'
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('pizza-finder')
+);
